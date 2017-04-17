f0aaf1369a49ec65c2d297010f23228178c94440
Florencio Natan <florencionatan@gmail.com>
2017-04-17 15:43:14 -0300

    Create migration for table friends.
    new file:   database/migrations/2017_04_17_181958_create_friends_table.php

---
+++ database/migrations/2017_04_17_181958_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user0_id')->unsigned();
+            $table->integer('user1_id')->unsigned();
+            $table->foreign('user0_id')->references('id')->on('users');
+            $table->foreign('user1_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
