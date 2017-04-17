a676934f8fe5312921300085ef65065b708b4a74
Florencio Natan <florencionatan@gmail.com>
2017-04-17 19:57:05 -0300

    Add DB seeder. modified:   database/seeds/DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,57 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('users')->insert([
+            'name' => 'Usuário da Rede Social',
+            'email' => 'user@social.com',
+            'password' => bcrypt('password'),
+            'remember_token' => str_random(100),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        $faker = Faker\Factory::create();
+        for ($i=0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->email,
+                'password' => bcrypt('secret'),
+                'remember_token' => str_random(100),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ]);
+        }
+
+        for ($i=0; $i < 10; $i++) {
+            DB::table('friends')->insert([
+                'user0_id' => 1,
+                'user1_id' => 2+$i,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ]);
+        }
+
+        for ($i=0; $i < 10; $i++) {
+            DB::table('posts')->insert([
+                'title' => $faker->name,
+                'text' => $faker->text(500),
+                'user_id' => 1,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ]);
+
+            for ($j=0; $j < 5; $j++) {
+                DB::table('comments')->insert([
+                    'text' => $faker->text(150),
+                    'post_id' => 1+$i,
+                    'user_id' => 2+$j,
+                    'created_at' => date("Y-m-d H:i:s"),
+                    'updated_at' => date("Y-m-d H:i:s"),
+                ]);
+            }
+        }
+
+
     }
 }
