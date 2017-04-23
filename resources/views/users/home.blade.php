97b3fc32298b94708ae8057d663650af508ff426
Florencio Natan <florencionatan@gmail.com>
2017-04-22 21:52:09 -0300

    Creating post dynamically using AngulariJS.
    modified:   resources/views/users/home.blade.php

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="{{ asset('js/angular-sanitize.js') }}"></script>
 <div class="container">
     <div class="row">
         <div class="col-md-8">
@@ -22,16 +23,47 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Posts</div>
 
-                <div class="panel-body">
-                    <?php foreach ($user->posts as $key => $post): ?>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <h2>{{ $post->title }}</h2>
-                                <p align="justify">{{ $post->text }}</p>
+                <div class="panel-body" ng-app="homeApp" ng-controller="homeCtrl">
+
+                    <form id="form-post"class="form-horizontal" role="form" method="POST" action="{{ route('post') }}">
+
+                        <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                            <label for="title" class="col-md-3 control-label">Title</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control" name="title" ng-model="title" value="{{ old('title') }}" required autofocus>
+
+                                @if ($errors->has('title'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
+
+                        <div class="col-md-8 col-md-offset-2">
+                            <textarea id="text" class="form-control" name="text" ng-model="text" form="form-post"></textarea>
+                            <br>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-8">
+                                <button type="button" class="btn btn-primary" ng-click="send()" >
+                                    &nbsp;&nbsp;&nbsp;Post&nbsp;&nbsp;&nbsp;
+                                </button>
+                            </div>
+                        </div>
+
                         <hr>
-                    <?php endforeach; ?>
+                    </form>
+
+                    <div class="row" ng-repeat="post in posts">
+                         <div class="col-md-12">
+                            <h2 ng-bind-html="post.title"></h2>
+                            <p align="justify" ng-bind-html="post.text"></p>
+                         </div>
+                       <hr>
+                    </div>
                 </div>
             </div>
         </div>
@@ -56,4 +88,44 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    var app = angular.module('homeApp', ['ngSanitize']);
+
+    app.controller('homeCtrl', function($scope, $http) {
+        $scope.text = "What's in your mind?";
+
+        var pst = "{{ $user->posts }}";
+        pst = pst.replace(/\n/g,"\\n");
+        $scope.posts = JSON.parse(pst.replace(/&quot;/g, "\""));
+
+        $scope.send = function(){
+            var data = $.param({
+                title: $scope.title,
+                text: $scope.text,
+                _token: "{{ csrf_token() }}"
+            });
+
+            var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+
+            $http.post('{{ route("post") }}', data, config)
+            .then(function (data, status, headers, config) {
+                var out = '';
+                $scope.title = '';
+                $scope.text = '';
+                $scope.posts.unshift(data['data']) ;
+            },
+            function (data, status, header, config) {
+                alert("Data: " + data +
+                    "\nstatus: " + status +
+                    "\nheaders: " + header +
+                    "\nconfig: " + config);
+            });
+        }
+    });
+</script>
 @endsection
